5385a3657d2b3ee567d502fcc6e41b3c2628bc0f
Nikita Chicherin <nickita,rom@gmail.com>
2022-12-31 21:40:59 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
