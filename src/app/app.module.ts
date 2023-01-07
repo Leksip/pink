4a8ad570dea165325e71c1fe42a5c57ef19bd44d
Nikita Chicherin <nickita,rom@gmail.com>
2023-01-07 22:49:13 +0300

    Сверстан блок с информацией

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    StartPageComponent
+    StartPageComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
